8d8173695e534026f994f8633dfd5bd82fea3bca
Maximilian Alba <albam476@gmail.com>
2023-03-28 20:07:23 -0400

    add error for invalid login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,11 @@ class SessionsController < ApplicationController
 
     def create
         user = User.find_by(username: params[:username])
-
         if user&.authenticate(params[:password])
             session[:user_id] = user.id
             render json: user, status: :created
+        else
+            render json: {error: "Invalid Username or password"}, status: :unauthorized
         end
     end
 
