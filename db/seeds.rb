85527b62ca65cd7138c0e3f24fc0d0d02217c26d
Maximilian Alba <albam476@gmail.com>
2023-04-09 19:11:43 -0400

    Add faker gem and create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(username:"test", password: "abcd1234", name:"Fig")
+User.create!(username:"spazzyCat", password: "abcd1234", password_confirmation: "abcd1234", name:"Fig", image_url: "http://clipart-library.com/img1/1108294.jpg", bio:"")
+User.create!(username:"upDog", password: "abcd1234", password_confirmation: "abcd1234", name: "Fido", image_url: "http://clipart-library.com/img1/1459326.png", bio:"" )
+
+
+15.times{User.create!(username:Faker::Internet.username, password: "abcd1234", password_confirmation: "abcd1234", name: Faker::Name.first_name, image_url:[ "http://clipart-library.com/img1/1108294.jpg", "http://clipart-library.com/img1/1459326.png"].sample , bio:"")}
+
+
 Resolution.create!(goal_statement: "Read More Books", category: "Mental")
-Pact.create!(specific?:true, goal_int: 3, resolution_id: Resolution.all.sample.id, user_id: User.all.sample.id, frequency_scope: "Monthly" )
+Resolution.create!(goal_statement: "Practice Guitar", category: "Mental")
+Resolution.create!(goal_statement: "Practice a New Recipe", category: "Mental")
+Resolution.create!(goal_statement: "Go to the Gym", category: "Physical")
+Resolution.create!(goal_statement: "Stretch", category: "Physical")
+Resolution.create!(goal_statement: "Eat Vegetables with a Meal", category: "Physical")
+Resolution.create!(goal_statement: "Spend Time with Friends", category: "Social")
+Resolution.create!(goal_statement: "Call a Family Member", category: "Social")
+Resolution.create!(goal_statement: "Visit Parents", category: "Social")
+
+User.all.each{|user| 4.times{user.pacts.create!(isSpecific: true, goal_int: rand(1..10), resolution_id: Resolution.all.sample.id)}}
+
+User.all.each{|user| 4.times{user.pacts.create!(isSpecific: false, goal_int: rand(1..10), resolution_id: Resolution.all.sample.id, frequency_scope: ["Daily", "Weekly", "Monthly"].sample)}}
