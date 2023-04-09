d9db31ff7e49012b875c50b489894fa80cbfc3f5
Maximilian Alba <albam476@gmail.com>
2023-04-09 19:12:22 -0400

    Change formatting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,9 @@ before_action :authorize
 private
 
 def authorize
+  
   @current_user = User.find_by(id:session[:user_id])
-
+  
   render json: {errors: ["Not authorized"]}, status: :unauthorized unless @current_user
 end
 
