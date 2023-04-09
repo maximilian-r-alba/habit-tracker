6259bc99ccaa547da364f06a573f2e05cbd93516
Maximilian Alba <albam476@gmail.com>
2023-04-09 19:15:33 -0400

    create pact cards and form

---
+++ client/src/Pacts/PactCard.js
@@ -0,0 +1,258 @@
+import { useEffect, useState } from "react"
+
+import { UserContext } from "../UserContext"
+import { useContext } from "react"
+import styled from "styled-components"
+import ProgressCircle from "./ProgressCircle"
+import ProgressSquare from "./ProgressSquare"
+import {MdAddTask, MdOutlinePermDeviceInformation} from "react-icons/md"
+import {AiOutlineEdit} from "react-icons/ai"
+import {TiDocumentDelete} from "react-icons/ti"
+
+
+function PactCard({pact , handlePacts , handlePactForm , deletePact}){
+
+    // console.log('pact card rendered')    
+    const currentUser = useContext(UserContext)
+    
+    const [pactInstance, setPactInstance] = useState(pact)
+    const [instanceDates, setInstanceDates] = useState(pact['progress_dates'])
+
+    useEffect(()=>{
+        setPactInstance(pact)
+    }, [pact])
+ 
+    const goal = pactInstance.goal_int
+  
+    const today = new Date(new Date("4/5/23").setHours(23,59,59,59))
+    const yesterday = new Date( new Date(today.valueOf()).setDate(today.getDate() -1))
+    const tomorrow = new Date( new Date(today.valueOf()).setDate(today.getDate() +1))
+    const weekStart = new Date( new Date(today.valueOf()).setDate(today.getDate() - today.getDay()))
+    const weekEnd = new Date( new Date(today.valueOf()).setDate(today.getDate() - today.getDay()+6))
+    
+    const weekArray = generateWeek(weekStart, weekEnd).map((day) => <ProgressSquare key={day.toString().slice(0,3)} progressDates={instanceDates} day={day} weekStart={weekStart} weekEnd={weekEnd}/>)
+   
+    function generateWeek(startDate, stopDate) {
+        const dateArray = new Array();
+        let currentDate = startDate;
+        while (currentDate <= stopDate) {
+            dateArray.push(new Date (currentDate));
+            const newDate = currentDate.getDate() + 1
+            currentDate = new Date (currentDate.setDate(newDate))
+          
+        }
+        return dateArray;
+    }
+
+
+    function willBeComplete(){
+        const progress = pactInstance.progress_dates.length
+
+       if (goal <= progress + 1 && pactInstance.isSpecific){
+            return true
+        }
+        return false
+    }
+
+    const pactParams = {pact_id: pactInstance.id, resolution_id: pactInstance.resolution.id, progressDate:today}
+    
+    useEffect(()=> {
+        
+            const filterOldProgress = (frequency_scope) =>{
+                switch(frequency_scope){
+                    case "Monthly":
+                        return pactInstance['progress_dates'].filter((obj) => {
+                                    const dateObj = new Date(Date.parse(obj.progressDate))
+                                    return dateObj.getYear() == today.getYear() && dateObj.getMonth() == today.getMonth()})
+                    case "Weekly":
+                        return pactInstance['progress_dates'].filter((obj) => {
+                            return Date.parse(weekStart) <= Date.parse(obj.progressDate) &&  Date.parse(obj.progressDate) <= Date.parse(weekEnd)})
+
+                    case "Daily":
+                        return pactInstance['progress_dates'].filter((obj) => {
+                                    return Date.parse(obj.progressDate) > Date.parse(yesterday)})
+                    default: 
+                        return pactInstance['progress_dates']
+                }
+
+            }
+            
+            setInstanceDates(filterOldProgress(pactInstance['frequency_scope']))
+       
+    }, [pactInstance])
+
+
+
+    function handlePactProgress(){
+        fetch('/progress_dates', {
+            method: "POST",
+            headers: {"Content-Type" : "application/json"},
+            body: JSON.stringify(pactParams)
+        }).then((r) =>{
+            if (r.ok){
+                r.json().then(progressObj => {
+                   
+                    if (willBeComplete()){
+                        handlePacts({...pactInstance, 'completion_date':today, 'progress_dates':[...pactInstance['progress_dates'], progressObj]})
+                        setPactInstance({...pactInstance, 'completion_date':today, 'progress_dates':[...pactInstance['progress_dates'], progressObj]})
+                      }
+                    else{
+                        handlePacts({...pactInstance, 'progress_dates':[...pactInstance['progress_dates'], progressObj]})
+                        setPactInstance({...pactInstance, 'progress_dates':[...pactInstance['progress_dates'], progressObj]})
+                        }
+                        setInstanceDates([...pactInstance['progress_dates'], progressObj])
+                    })
+                    }})
+            }
+            
+    function handleDeletePact(){
+        fetch(`/pacts/${pactInstance.id}`, {
+            method: "DELETE"
+        }).then((r) => {
+            if(r.ok){
+               
+                handlePacts(pactInstance, "DELETE")
+            }
+        })
+
+    }
+    
+    return <>
+    
+      {!pactInstance['completion_date'] || Date.parse(yesterday) < Date.parse(pactInstance['completion_date']) && Date.parse(pactInstance['completion_date']) < Date.parse(tomorrow) ?  
+      
+      <StyledMilestone category={pactInstance.frequency_scope} className={`${pactInstance.frequency_scope}`}>
+            <span><p className="category">{pact.resolution.category}</p>{ currentUser.id === pactInstance.user_id ?  <><AiOutlineEdit onClick={() => handlePactForm(pactInstance.resolution , pactInstance)}/> <TiDocumentDelete onClick={handleDeletePact} /> </> : <></>}</span>
+
+            {/* {currentUser.id == pactInstance.user_id ? <TiDocumentDelete onClick={handlePactDelete} /> : <></>} */}
+            <h2>{pact.resolution.goal_statement}</h2>
+            
+            {pactInstance.frequency_scope == "Weekly" ?  
+            <>
+            <p>{goal} per week</p>
+            <div className='weekArray'>{weekArray}</div>
+            </>
+             : 
+            
+            <>
+                <ProgressCircle className="circle" percentage={(instanceDates.length/goal)*100} color={"red"} />
+                <p className='fraction'>{instanceDates.length}/{goal}</p>
+            </> }
+            
+
+            { goal <= instanceDates.length ? 
+            
+            <p className="completed">{pactInstance['completion_date'] ? 
+            
+            ` Completed on: ${new Date(Date.parse(pactInstance['completion_date'])).toLocaleDateString("en-US")}` : "Completed"}
+            
+            </p> 
+            
+            : <MdAddTask className="progressBtn" onClick={handlePactProgress} />}
+            
+        </StyledMilestone> : <></>}
+       
+    
+       
+    </>
+  
+}
+
+export default PactCard
+
+const StyledMilestone = styled.div`
+border: solid;
+border-radius:25px;
+margin: 10px;
+position: relative;
+flex: none;
+
+h2, p{
+    padding-left: 5px;
+}
+
+p.category{
+    
+    border-style: none none solid none;
+}
+
+${props => props.category == 'Monthly' ? MonthlyCSS : props.category == 'Weekly' ? WeeklyCSS : DailyCSS}
+
+
+
+`
+const WeeklyCSS = `
+height: 20vh;
+
+
+div{
+    position: absolute;
+    right: 25%;
+    bottom: 25%;
+    font-size: 20px;
+    *{
+        padding-left: 10px;
+    }
+}
+
+
+svg.progressBtn{
+    position: absolute;
+    bottom: 1vh;
+    right: 1vw;
+    font-size: 20px;
+ }
+`
+
+const MonthlyCSS = `
+width: 20vw;
+
+p.completed{
+    padding-top: 40px;
+   
+}
+
+.progressCircle{
+    position: absolute;
+    top: 30%;
+    right: 0;
+}
+p.fraction{
+        position: absolute;
+        right: 8vw;
+        bottom: 0vh;
+    }
+
+    svg.progressBtn{
+        position: absolute;
+        bottom: 1.5vh;
+        right: 1vw;
+        font-size: 20px;
+     }
+`
+const DailyCSS = `
+height: 20vh;
+
+.progressCircle{
+    position: absolute;
+    top: 10%;
+    right: 0;
+}
+p.fraction{
+    position: absolute;
+    right: 8vw;
+    bottom: 0;
+}
+
+p.completed{
+    padding-top: 20px;
+   
+}
+
+svg.progressBtn{
+   position: absolute;
+   bottom: 1.5vh;
+   right: 1vw;
+   font-size: 20px;
+}
+`
---
+++ client/src/Pacts/PactForm.js
@@ -0,0 +1,189 @@
+import { useContext, useEffect, useState } from "react"
+import { UserContext } from "../UserContext"
+
+import {AiOutlineClose} from "react-icons/ai"
+import styled from "styled-components"
+
+function PactForm({resolution , handlePacts, setShowPactForm , pact}){
+
+    // console.log('pact form rendered')
+    const user = useContext(UserContext)
+   
+    const [pactParams, setPactParams] = useState({frequency_scope: undefined, goal_int: 1, isSpecific: true,resolution_id: resolution.id})
+    const [errorData, setErrorData] = useState()
+
+    useEffect(() => {
+        if(pact){
+            console.log('pact was preset')
+            setPactParams({frequency_scope: pact.frequency_scope, goal_int: pact.goal_int, isSpecific: pact.isSpecific, resolution_id: pact.resolution.id})
+        }
+    }, [])
+
+    
+    function handleSubmit(e){
+        e.preventDefault()
+        if(pact){
+            console.log('patch')
+            fetch(`/pacts/${pact.id}`, {
+                method: "PATCH",
+                headers: {"Content-Type" : "application/json"},
+                body: JSON.stringify(pactParams)}).then((r) =>{
+                    if(r.ok){
+                        r.json().then(handlePacts)
+                        setShowPactForm(false)
+                    }
+                    else{
+                        handlePacts(undefined)
+                        r.json().then(setErrorData)
+                    }
+                })
+            }
+
+        
+        else{
+            
+            fetch('/pacts', {
+            method: "POST",
+            headers: {"Content-Type" : "application/json"},
+            body: JSON.stringify(pactParams)
+        }).then(r => {
+      
+            if(r.ok){
+                r.json().then(pact => handlePacts(pact))
+                setShowPactForm(false)
+            }
+        })
+    }
+        
+    }
+
+    function handleChange(e){
+        const key = e.target.name
+        const value = e.target.value
+
+        switch(key){
+            case 'goal_int':
+            setPactParams({...pactParams, [key]: parseInt(value)})
+                break;
+
+            case 'isSpecific':
+                if(value==='true'){
+                    setPactParams({...pactParams, [key]: true, ['frequency_scope'] : false})
+                }
+                else{
+                    setPactParams({...pactParams, [key]: false})
+                }
+                break;
+
+            default:
+                setPactParams({...pactParams, [key]: value})
+                break;
+        } 
+    }
+
+
+    return (
+ <>
+ 
+ { errorData ? <ErrorsStyled>Uh-oh:<p>{errorData.error}</p>
+ </ErrorsStyled> : <></>}
+
+
+        <StyledPactForm onSubmit={handleSubmit}>
+        
+<AiOutlineClose onClick={() => setShowPactForm(false)}/>
+<label>
+    <h3>{resolution.goal_statement}</h3>
+    Is your goal a specific achievement or a repeated process?
+   
+    <select name="isSpecific" value = {pactParams['isSpecific']} onChange={handleChange}>
+        <option value = 'true'>Specific</option>
+        <option value = "false">Repeated</option>
+    </select>
+
+</label>
+
+{pactParams['isSpecific'] ? 
+<label>
+    What is your target number?
+    <input type = "number" name="goal_int" value = {pactParams['goal_int']} min = "1" onChange={handleChange} ></input>
+</label> 
+: 
+    <>
+    <label>
+
+    Track goal by: 
+
+    <select name="frequency_scope" value = {pactParams['frequency_scope']} onChange={handleChange}>
+        <option value = {undefined}></option>
+        <option value = "Daily">Day</option>
+        <option value = "Weekly">Week</option>
+        <option value = "Monthly">Month</option>
+    </select>
+
+    </label>
+
+    <label>
+    How often?
+    <input type = "number" name="goal_int" value = {pactParams['goal_int']}min = "1" onChange={handleChange} ></input>
+    </label> 
+            </>
+    
+        }
+    
+<input type="submit"/>
+</StyledPactForm>
+
+</>
+    )
+}
+
+
+export default PactForm
+
+const StyledPactForm = styled.form`
+display: flex;
+flex-direction: column;
+gap: 1vh;
+align-items: center;
+border: solid;
+border-radius: 25px;
+width: 20vw;
+position: relative;
+*{
+    margin: 5px;
+}
+
+.submitBtn{
+    border: none;
+    background: none;
+    font-size: 20px;
+    text-decoration: underline;
+    padding-bottom: 20px;
+    cursor: pointer;
+}
+
+svg{
+    position: absolute;
+    bottom: 2%;
+    right: 2%;
+    cursor: pointer;
+}
+`
+
+const ErrorsStyled = styled.div`
+color: red;
+border: solid;
+border-radius: 25px;
+text-align: center;
+word-wrap: break-word;
+padding: 20px;
+display: inline-block;
+font-size: 20px;
+width: 15vw;
+margin-left: auto;
+margin-right: auto;
+position: relative;
+top: 15vh;
+left: 25vw;
+`
