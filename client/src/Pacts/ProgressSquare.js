efbd1144020bb9cf7103de3925a5f38975503939
Maximilian Alba <albam476@gmail.com>
2023-04-09 19:16:25 -0400

    create components for progress tracking

---
+++ client/src/Pacts/ProgressSquare.js
@@ -0,0 +1,28 @@
+
+import {BiCheckbox , BiCheckboxChecked} from "react-icons/bi"
+function ProgressSquare({day, progressDates, weekEnd}){
+    // console.log('progress square rendered')
+    const boxDate = day.toLocaleDateString('en-US')
+    const dayOfWeek = day.toString().slice(0,3)
+    const weekStart = new Date(new Date( new Date(day.valueOf()).setDate(day.getDate() - day.getDay())).setHours(0,0,0,0))
+
+    const filteredDates = progressDates.filter((obj) => {
+        
+        const dateObj = new Date (Date.parse(obj.progressDate))
+     
+        return weekStart < dateObj && dateObj < weekEnd
+    }).map((dateObj) => new Date(dateObj.progressDate).toLocaleDateString('en-US'))
+    
+    
+
+    return <>
+        <label>
+        <span>{dayOfWeek}</span>
+        {filteredDates.includes(boxDate) ? <span><BiCheckboxChecked />x{filteredDates.filter(date => date == boxDate).length}</span> :  <BiCheckbox />}
+        </label>
+        
+    </>
+}
+
+export default ProgressSquare
+
