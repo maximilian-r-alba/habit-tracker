d1f2cfb64277240d9397875b477cfbdaf1776f71
Maximilian Alba <albam476@gmail.com>
2023-04-09 23:00:38 -0400

    remove comment

--- app/serializers/pact_serializer.rb
+++ app/serializers/pact_serializer.rb
@@ -1,7 +1,5 @@
 class PactSerializer < ActiveModel::Serializer
   attributes :id, :completion_date, :isSpecific, :goal_int, :frequency_scope, :user_id
-
-  # does it need user_id?
   belongs_to :resolution
   has_many :progress_dates
   
