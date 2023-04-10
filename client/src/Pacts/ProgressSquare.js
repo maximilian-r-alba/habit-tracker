d3a9a8667e56ffb45d39f45d84161a66a8e21de8
Maximilian Alba <albam476@gmail.com>
2023-04-10 00:04:14 -0400

    remove console logs

--- client/src/Pacts/ProgressSquare.js
+++ client/src/Pacts/ProgressSquare.js
@@ -5,7 +5,7 @@ function ProgressSquare({day, progressDates, weekEnd}){
     const boxDate = day.toLocaleDateString('en-US')
     const dayOfWeek = day.toString().slice(0,3)
     const weekStart = new Date(new Date( new Date(day.valueOf()).setDate(day.getDate() - day.getDay())).setHours(0,0,0,0))
-    console.log(weekStart)
+ 
     const filteredDates = progressDates.filter((obj) => {
         
         const dateObj = new Date (Date.parse(obj.progressDate))
