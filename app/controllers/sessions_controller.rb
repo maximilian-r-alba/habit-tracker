33433935229daddafb5678b8e955a3149ab564ab
Maximilian Alba <albam476@gmail.com>
2023-04-09 23:00:14 -0400

    edit spacing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
 
     def create
         user = User.find_by(username: params[:username])
-
         if user&.authenticate(params[:password])
             session[:user_id] = user.id
             render json: user, include: ['pacts', 'pacts.resolution', 'pacts.progress_dates', 'resolutions'] , status: :created
