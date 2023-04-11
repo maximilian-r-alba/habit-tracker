91775ceb4f262466b31fd8e99a1d832ce8a3dfb8
Maximilian Alba <albam476@gmail.com>
2023-04-10 21:09:56 -0400

    ensure only logged in user can delete itself

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
     end
 
     def destroy 
-        user = User.find(params[:id])
+        user = User.find(session[:user_id])
         user.destroy
         head :no_content
     end
